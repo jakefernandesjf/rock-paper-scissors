d74d87fdf7aa0b1e9089298dad83f57005e7bb2a
jakefernandesjf <46454692+jakefernandesjf@users.noreply.github.com>
2025-07-22 18:37:14 -0700

    Complete playGame() function

--- javascript.js
+++ javascript.js
@@ -2,23 +2,44 @@
 javascript.js
 */
 
-let humanScore = 0;
-let computerScore = 0;
-
-playRound(getHumanChoice(), getComputerChoice());
+playGame(5);
 
 
+function playGame(rounds) {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    console.log(`PLAYING ROCK, PAPER, SCISSORS FOR ${rounds} rounds . . .`);
+    for (let round = 0; round < rounds; round++) {
+        console.log(`ROUND ${round + 1} . . .`);
+        let result = playRound(getHumanChoice(), getComputerChoice());
+        if (result === true) {
+            humanScore++;
+        } else if (result === false) {
+            computerScore++;
+        }
+        console.log(`CURRENT SCORE: YOU: ${humanScore} COMPUTER: ${computerScore}`);
+    }
+    if (humanScore == computerScore) {
+        console.log(`FINAL RESULT: TIE!`);
+    } else if (humanScore > computerScore) {
+        console.log(`FINAL RESULT: YOU WIN!`);
+    } else {
+        console.log(`FINAL RESULT: YOU LOSE!`);
+    }
+}
 
 function playRound(humanChoice, computerChoice) {
     console.log(`COMPUTER PICKS ${computerChoice.toUpperCase()}!`)
     if (humanChoice === computerChoice) {
         console.log(`Tie! You both picked ${humanChoice}!`);
+        return null;
     } else if (isWinner(humanChoice, computerChoice)) {
         console.log(`You win! Your ${humanChoice} beats ${computerChoice}!`);
-        humanScore++;
+        return true;
     } else {
         console.log(`You lose! Your ${humanChoice} loses to ${computerChoice}.`);
-        computerScore;
+        return false;
     }
 }
 
